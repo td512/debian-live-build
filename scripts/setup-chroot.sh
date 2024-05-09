c7e041aac942ee2fb443a2628a0bb66ea4a916b3
Kid <github@theom.nz>
2024-05-10 06:48:42 +1200

    introduce native arm64 support

--- scripts/setup-chroot.sh
+++ scripts/setup-chroot.sh
@@ -6,5 +6,4 @@ sudo mount --make-rslave --rbind /proc debian/proc
 sudo mount --make-rslave --rbind /sys debian/sys
 sudo mount --make-rslave --rbind /dev debian/dev
 sudo mount --make-rslave --rbind /run debian/run
-sudo cp "$(which qemu-aarch64-static)" debian/usr/bin
 popd
