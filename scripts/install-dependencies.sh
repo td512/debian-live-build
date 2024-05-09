ce415b55a2cc76ee120912c6151035ccd55b68a7
Kid <github@theom.nz>
2024-05-10 06:16:41 +1200

    let's not upgrade every time

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install qemu-user-static debootstrap qemu-utils qemu-efi-aarch64 qemu-system-arm xz-utils -y
+sudo apt-get update && sudo apt-get install qemu-user-static debootstrap qemu-utils qemu-efi-aarch64 qemu-system-arm xz-utils -y
 sudo docker run --privileged --rm tonistiigi/binfmt --install all
